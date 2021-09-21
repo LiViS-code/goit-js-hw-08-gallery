b49582335527e0cd215f4c75874bee6d55cd83e6
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-09-21 23:15:03 +0300

    основной функционал

---
+++ js/create-gallery.js
@@ -0,0 +1,57 @@
+import * as moduleGallery from "./app.js";
+
+const ref = {
+  gallery: document.querySelector(".js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  btnClose: document.querySelector('button[data-action="close-lightbox"]'),
+  lightboxImage: document.querySelector(".lightbox__image"),
+};
+
+const onClick = (e) => {
+  if (e.target.nodeName === "IMG") e.preventDefault();
+  ref.lightboxImage.src = e.target.dataset.source;
+  ref.lightboxImage.alt = e.target.alt;
+};
+
+const openModal = () => {
+  ref.lightbox.classList.add("is-open");
+  ref.gallery.removeEventListener("click", openModal);
+  ref.btnClose.addEventListener("click", closeModal);
+};
+
+const closeModal = () => {
+  ref.lightbox.classList.remove("is-open");
+  ref.btnClose.removeEventListener("click", closeModal);
+  ref.gallery.addEventListener("click", openModal);
+  ref.lightboxImage.src = "";
+  ref.lightboxImage.alt = "";
+};
+
+// сформировать массив шаблонных строк с разметкой согласно шаблона
+const imgMarkup = new Array(moduleGallery.galleryItems.length)
+  .fill(0)
+  .map((_, i) => {
+    return `
+    <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href=${moduleGallery.galleryItems[i].original}
+    >
+      <img
+        class="gallery__image"
+        src=${moduleGallery.galleryItems[i].preview}
+        data-source=${moduleGallery.galleryItems[i].original}
+        alt=${moduleGallery.galleryItems[i].description}
+      />
+    </a>
+  </li>
+  `;
+  })
+  .join("");
+
+// рендер разметки в DOM
+ref.gallery.insertAdjacentHTML("afterbegin", imgMarkup);
+
+// делегируем нажатия на элементах галереи
+ref.gallery.addEventListener("click", onClick);
+ref.gallery.addEventListener("click", openModal);
